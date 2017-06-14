e145ce1eaa54c912faf37ff55feeccc4bffd17e1
Ender Loc Phan <weintome@gmail.com>
2017-06-14 12:25:33 +0300

    format and function added

--- adTracking.ps1
+++ adTracking.ps1
@@ -1,13 +1,12 @@
 #Dev by Ender Loc Phan
 
-param(
-
+param (
 [int]$amount,
 [switch]$dna,
 [switch]$userex,
 [switch]$userstatus
-
 )
+
 Write-Verbose -Message  "This script is running under PowerShell version $($PSVersionTable.PSVersion.Major)" -Verbose
 
 $Domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
@@ -28,14 +27,20 @@ $ADSearch.Filter = "(objectClass=user)"
 # value of attribute : user
 #exp: $ADSearch.Filter = "(Name=Ender)"
 
- 
-$properies = "distinguishedName","sAMAccountName","mail","lastLogonTimeStamp","pwdLastSet","accountExpires","userAccountControl"
+
+$properies =@("distinguishedName",
+"sAMAccountName",
+"mail",
+"lastLogonTimeStamp",
+"pwdLastSet",
+"accountExpires",
+"userAccountControl")
 #values in array are atttibute of LDAP
 
 
 
-foreach($pro in $properies){
-
+foreach($pro in $properies)
+{
     $ADSearch.PropertiesToLoad.add($pro)
     #the name of property of the object, search will load the name in an array #properties
 }
@@ -45,129 +50,148 @@ $ProgressBar = $True
 $userObjects = $ADSearch.FindAll()
 $userCount =  $userObjects.Count
 $result = @()
-$count = 0;
+$count = 0
+
 $csvFileName = "Y:\Powershell\adTracking.csv"
 $userValue = @("512",
-                "514",
-                "544",
-                "546",
-                "66048",
-                "66050",
-                "66080",
-                "66082",
-                "262656",
-                "262658",
-                "262688",
-                "262690",
-                "328192",
-                "328194",
-                "328224",
-                "328226")
+"514",
+"544",
+"546",
+"66048",
+"66050",
+"66080",
+"66082",
+"262656",
+"262658",
+"262688",
+"262690",
+"328192",
+"328194",
+"328224",
+"328226")
+
+
+
+
+Function tracking
+{
+    $dn =  $user.Properties.Item("distinguishedName")
+    
+    $global:sam = $user.Properties.Item("sAMAccountName")
+    $logon = $user.Properties.Item("lastLogonTimeStamp")
+    $mail =$user.Properties.Item("mail")
+    $passwordLS = $user.Properties.Item("pwdLastSet")
+    $accountEx = $user.Properties.Item("accountExpires")
+    $accountDis= $user.Properties.Item("userAccountControl")
+    
+    if($logon.Count -eq 0)
+    {
+        $lastLogon = "Never"
+    }
+    else
+    {
+        $lastLogon = [DateTime]$logon[0]
+    }
+    
+    $value = [DateTime]::FromFileTime($passwordLS[0])
+    
+    if($accountEx.accountExpires -eq 0)
+    {
+        $convertAccountEx = "Never"
+    }
+    else
+    {
+        $convertAccountEx = [DateTime]::FromFileTime($AccountEx.accountExpires)
+    }
+    
+    if($accountDis -eq 512)
+    {
+        $accountDisStatus = "User is disbled"
+    }else
+    {
+        $accountDisStatus = "User ready for logon"
+    }
+    
+    
+    """$dn"",$sam,$mail,'password change: '$value,'last logon: '$lastLogon,'account expired: ' $convertAccountEx,'account status: '$accountDisStatus"
+    Write-Host
+}
 
 ## distinguished Name method
 $arrayDN = @()
-if($dna){    
-    
-    if($amount){
-            
+
+if($dna)
+{
+    if($amount)
+    {
         Write-Verbose -Message  "Please be patient whilst the script retrieves all $amount distinguished names..." -Verbose        
-       
-        foreach ($user  in $userObjects){        
-            If ($ProgressBar) {
-                Write-Progress -Activity "Processing $($amount) Users" -Status ("Count: $($TotalUsersProcessed)- Username: {0}" -f $sam) -PercentComplete (($TotalUsersProcessed/$amount)*100)
-            }
-            if($count -lt $amount){
+        
+        foreach ($user  in $userObjects)
+        {
+            if($count -lt $amount)
+            {
                 $sam = $user.Properties.Item("sAMAccountName")
                 $dn =  $user.Properties.Item("distinguishedName")
                 $arrayDN += $dn 
                 $count++    
-                $TotalUsersProcessed++          
-            }     
-        }
-        $arrayDN
-   
-    }    
-    else{
-    Write-Verbose -Message  "Please be patient whilst the script retrieves all $userCount distinguished names..." -Verbose
-        foreach ($user  in $userObjects){
-                If ($ProgressBar) {
-                    Write-Progress -Activity "Processing $($userCount) Users" -Status ("Count: $($TotalUsersProcessed)- Username: {0}" -f $sam) -PercentComplete (($TotalUsersProcessed/$userCount)*100)
-                }
-                $sam = $user.Properties.Item("sAMAccountName")
-                $dn =  $user.Properties.Item("distinguishedName")
-                $arrayDN += $dn
-                $TotalUsersProcessed++
+                $TotalUsersProcessed++   
+                $sam
             }
+            If ($ProgressBar) 
+            {
+                Write-Progress -Activity "Processing $($amount) Users" -Status ("Count: 
+                $($TotalUsersProcessed)- Username: {0}" -f $sam) -PercentComplete (($TotalUsersProcessed/$amount)*100)
+            }
+        }
+        $arrayDN
+    }    
+    else
+    {
+        Write-Verbose -Message  "Please be patient whilst the script retrieves all $userCount distinguished names..." -Verbose
+        foreach ($user  in $userObjects)
+        {
+            $sam = $user.Properties.Item("sAMAccountName")
+            $dn =  $user.Properties.Item("distinguishedName")
+            $arrayDN += $dn
+            $TotalUsersProcessed++
+            If ($ProgressBar) 
+            {
+                Write-Progress -Activity "Processing $($userCount) Users" -Status ("Count: 
+                $($TotalUsersProcessed)- Username: {0}" -f $sam) -PercentComplete (($TotalUsersProcessed/$userCount)*100)
+            }
+        }
         
         $arrayDN
     }
-    
 }
-## Finish distinguished Name method
+## Finished distinguished Name method
 
 
-else{
-foreach ($user  in $userObjects){
-
-    if($count -lt 10){
-        <#
-        $thisObject = New-Object System.Object
-        foreach ($property in $user.Properties.PropertyNames){
-
-
-            $thisObject | Add-Member -type NoteProperty -Name $property -Value $user.Properties.Item($property)
-        }
-           
-        $result += $thisObject
-        #>
-        $dn =  $user.Properties.Item("distinguishedName")
-        
-        $sam = $user.Properties.Item("sAMAccountName")
-        $logon = $user.Properties.Item("lastLogonTimeStamp")
-        $mail =$user.Properties.Item("mail")
-        $passwordLS = $user.Properties.Item("pwdLastSet")
-        $accountEx = $user.Properties.Item("accountExpires")
-        $accountDis= $user.Properties.Item("userAccountControl")
-
-        if($logon.Count -eq 0){
-            $lastLogon = "Never"
-        }
-        else{
-        
-            $lastLogon = [DateTime]$logon[0]
-
-        }
-
-        $value = [DateTime]::FromFileTime($passwordLS[0])
-
-        if($accountEx.accountExpires -eq 0){
-                $convertAccountEx = "Never"
-        }
-        else{
-                $convertAccountEx = [DateTime]::FromFileTime($AccountEx.accountExpires)                
-        }
-        
-        if($accountDis -eq 512){
-                $accountDisStatus = "User is disbled"
-        }else{
-                $accountDisStatus = "User ready for logon"
-
-        }
-        
-        #$accountDis
-       
-        if(($dn -match "OU=EE" )){
-
-            #$result +=$dn,$sam,$mail,$value,$lastLogon,$convertAccountEx
-
-           """$dn"",$sam,$mail,'password change: '$value,'last logon: '$lastLogon,'account expired: ' $convertAccountEx,'account status: '$accountDisStatus"
-           $count++;       
-       
+elseif($amount)
+{
+    Write-Verbose -Message  "Please be patient whilst the script retrieves all $amount distinguished names..." -Verbose
+    foreach ($user  in $userObjects)
+    {
+        if($count -lt $amount)
+        {
+            tracking
+            $count++
         }
     }
-    
-    
 }
+else
+{
+    Write-Verbose -Message  "Please be patient whilst the script retrieves all $userCount distinguished names..." -Verbose
+    foreach ($user  in $userObjects)
+    {
+        If ($ProgressBar) 
+        {
+            tracking
+            Write-Progress -Activity "Processing $($userCount) Users" -Status ("Count: 
+            $($TotalUsersProcessed)- Username: {0}" -f $sam) -PercentComplete (($TotalUsersProcessed/$userCount)*100)
+            $TotalUsersProcessed++
+        }
+    }
 }
 #$result
 
